6416b94223a4482927fa37880d5fd19fc0d374fd
Nhu Van Duy <nhuvanduy97@gmail.com>
2019-07-30 01:09:16 +0700

    update2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
 import UserService from './services/UserService'
-import { Navbar, Nav } from 'react-bootstrap'
 import RouterURL from './config/RouterURL'
 import { BrowserRouter as Router } from "react-router-dom";
-import logo from './resources/img/user.png'
+import user from './resources/img/user.png'
+import logo from './resources/img/reserve.png'
+// import logo from './resources/img/Reservation.png'
+import { Nav, NavDropdown, Navbar } from 'react-bootstrap'
 
 export default class App extends Component {
   componentDidMount() {
@@ -17,26 +19,41 @@ export default class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <Navbar bg="primary" variant="dark">
-            <Navbar.Brand href="/">Rom Reservation</Navbar.Brand>
+
+        <Navbar bg="light" expand="lg">
+          {/* <Navbar.Brand href="#home">Room Reservation</Navbar.Brand> */}
+          <Navbar.Brand href="#home">
+            <img
+              src={logo}
+              width="150"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#home">List Room</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link href="/">NhuVanDuy</Nav.Link>
+              <Nav.Link href="/">List Rom</Nav.Link>
+              <Nav.Link href="#link">Book Room</Nav.Link>
+              <NavDropdown title="Account" id="basic-nav-dropdown">
+                {/* <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item> */}
+                <Navbar.Brand href="#home" style={{ display: 'flex', padding:'10px' }}>
+                  <img
+                    src={user}
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                    alt=""
+                  />
+                  <Nav style={{ color: 'blue', fontSize:'15px', marginLeft:'10px'}}><u>nhuvanduy97</u></Nav>
+                </Navbar.Brand>
+                <NavDropdown.Item href="#action/3.2"><u>Logout</u></NavDropdown.Item>
+              </NavDropdown>
             </Nav>
-            <Navbar.Brand>
-              <img
-                src={logo}
-                width="30"
-                height="30"
-              />
-            </Navbar.Brand>
-            <Nav>
-              <a style={{color:'white'}} href="#"><u>DuyNhu</u></a>
-            </Nav>
-          </Navbar>
-        </div>
+          </Navbar.Collapse>
+        </Navbar>
         <div className="container">
           <RouterURL />
         </div>
