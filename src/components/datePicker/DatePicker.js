c4332e1ecbc3b8fabe2bb943f0368893f887587f
Nhu Van Duy (VTI.D2.Intern) <duy.nhuvan@vti.vn>
2019-08-01 14:59:03 +0700

    refs #2 duy:update (8/1/2019)

--- src/components/datePicker/DatePicker.js
+++ src/components/datePicker/DatePicker.js
@@ -3,33 +3,36 @@ import { DatePicker, DatePickerInput } from 'rc-datepicker';
 import 'rc-datepicker/lib/style.css';
 import './datePicker.css'
 export default class DatePickerr extends React.Component {
-    constructor(props, context) {
-        super(props, context);    
-        this.state = {
-            selectedDate: new Date()
-        };
-        this.onChange = this.onChange.bind(this);
-    }
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      selectedDate: new Date()
+    };
+    this.onChange = this.onChange.bind(this);
+    console.log(this.state.selectedDate);
+  }
 
-    onChange(date) {
-		this.setState({
-			selectedDate: date
-		});
-	}
-    render() {
-        return (
-            <div className="datePicker">
-                <div>
-                    <h4 className="title">Booking Details</h4>
-                    <hr style={{borderColor:'white'}}/>
-                    <h5 className="title1">Date: </h5>
-                </div>
-                <DatePickerInput
-                    onChange={this.onChange}
-                    value={this.state.selectedDate}
-                />
-                <DatePicker onChange={this.onChange} value={this.state.selectedDate} />
-            </div>
-        );
-    }
+  onChange(date) {
+    this.setState({
+      selectedDate: date
+    });
+    console.log(date)
+  }
+ 
+  render() {
+    return (
+      <div className="datePicker">
+        <div>
+          <h4 className="title">Booking Details</h4>
+          <hr style={{ borderColor: 'white' }} />
+          <h5 className="title1">Date: </h5>
+        </div>
+        <DatePickerInput
+          onChange={this.onChange}
+          value={this.state.selectedDate}
+        />
+        <DatePicker className="calendar" onChange={this.onChange} value={this.state.selectedDate} />
+      </div>
+    );
+  }
 }
