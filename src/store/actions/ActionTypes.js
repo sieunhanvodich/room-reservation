c3d55fe207be6ef25214a30a91991ba60fe733aa
td20131998 <td20131998@gmail.com>
2019-08-01 14:56:45 +0700

    refs #2 update 8/1

--- src/store/actions/ActionTypes.js
+++ src/store/actions/ActionTypes.js
@@ -1,2 +1,8 @@
 export const SAVE_USER = 'SAVE_USER';
 
+export const SHOW_MODAL = 'SHOW_MODAL';
+export const CLOSE_MODAL = 'CLOSE_MODAL';
+export const GET_DETAIL_MEETING = 'GET_DETAIL_MEETING';
+export const ADD_MEMBER = 'ADD_MEMBER';
+export const REMOVE_MEMBER = 'REMOVE_MEMBER';
+export const REMOVE_MEETING = 'REMOVE_MEETING';
