e53807c05dbd34c0f7ee54ff3eb9cac3e50f56cb
darkflamemaster2321998 <darkflamemaster2321998@gmail.com>
2019-08-02 17:07:22 +0700

    refs #1 Xóa các cmt không cần thiết

--- src/view/login/Login.js
+++ src/view/login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container,Button, Row, Col } from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
 import './Login.css';
 
 class Login extends Component {
