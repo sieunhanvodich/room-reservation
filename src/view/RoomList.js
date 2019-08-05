5aefae6f5e1f9180b911da263b6a21f9bb006a02
Le Thanh Dat (VTI.D2.Intern) <dat.lethanh@vti.vn>
2019-08-05 10:44:12 +0700

    refs #5 Update

--- src/view/RoomList.js
+++ src/view/RoomList.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import Contents from '../components/roomlist/Contents';
 import Datepickerr from '../components/roomlist/Datepicker'
 import {Button} from 'react-bootstrap'
+import './RoomList.css'
+import RcQueueAnim from 'rc-queue-anim';
 
 
 class RoomList extends Component {
   render() {
     return (
+     
       <div>
-        <h1>List Room</h1>
-        <div>
+      <RcQueueAnim delay={200}>
+        <h1 key="1" className="headname">ROOM LIST</h1>
+        <div key="2">
           <Datepickerr />
-          <Button variant="primary">Submit</Button>
+          <Button className="button" variant="info">Submit</Button>
         </div>
-        <Contents />
+        <Contents key="3"/>
+        </RcQueueAnim>
       </div>
+      
     );
   }
 }
