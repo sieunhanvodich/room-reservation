0119de2a4a641b111f2ea0b583242ce3d1deb6aa
darkflamemaster2321998 <darkflamemaster2321998@gmail.com>
2019-08-05 14:18:43 +0700

    refs #1 xoa comment

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import './App.css'
 import UserService from  './services/UserService'
 import { connect } from 'react-redux'
 import { saveUserInfo } from './store/actions'
-// import Login from './view/login/Login'
 import { BrowserRouter as Router} from "react-router-dom";
 import RouterURL from './config/RouterURL'
-// import BookingScreen from './view/bookingScreen/BookingScreen.js'
 
 class App extends Component {
   componentDidMount() {
@@ -20,9 +18,6 @@ class App extends Component {
   
   render() {
     return (
-      // <div className="App">
-      //   Helloo
-      // </div>
       <Router>
           <RouterURL></RouterURL>
       </Router>
