ddc072c55eb379eeb64a65d5e86f8b0257a25920
darkflamemaster2321998 <darkflamemaster2321998@gmail.com>
2019-08-06 17:40:09 +0700

    refs #1 test api login

--- src/view/login/Login.js
+++ src/view/login/Login.js
@@ -1,8 +1,45 @@
 import React, { Component } from 'react';
-import {Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import './Login.css';
+import axios from 'axios';
 
 class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: "",
+      password: ""
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const form = {
+      username: this.state.username,
+      password: this.state.password
+    }
+
+    axios({
+      method: 'post',
+      url: 'http://10.1.45.111:3000/login',
+      data: form
+    })
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+        
+      });
+  }
+
   render() {
     return (
       <div className="container justify-content-center align-self-center d-flex h-100">
@@ -17,20 +54,20 @@ class Login extends Component {
                   <div className="input-group-prepend">
                     <span className="input-group-text"><i className="fas fa-user"></i></span>
                   </div>
-                  <input type="text" className="form-control" placeholder="Username" />
+                  <input type="text" className="form-control" onChange={e => this.handleChange(e)} name='username' placeholder="Username" />
 
                 </div>
                 <div className="input-group form-group">
                   <div className="input-group-prepend">
                     <span className="input-group-text"><i className="fas fa-key"></i></span>
                   </div>
-                  <input type="password" className="form-control" placeholder="Password" />
+                  <input type="password" className="form-control" onChange={e => this.handleChange(e)} name='password' placeholder="Password" />
                 </div>
                 <div className="row align-items-center remember">
                   <input type="checkbox" />Remember Me
 					</div>
                 <div className="form-group">
-                  <Button variant="outline-primary" className="submit login_btn float-right">Login</Button>
+                  <Button variant="outline-primary" className="submit login_btn float-right" onClick={(e) => this.onSubmit(e)}>Login</Button>
                 </div>
               </form>
             </div>
