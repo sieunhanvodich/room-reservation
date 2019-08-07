a041004fc238c557090d219eb5c10ec9ca57675e
darkflamemaster2321998 <darkflamemaster2321998@gmail.com>
2019-08-07 15:27:26 +0700

    refs #4 add compoent message

---
+++ src/components/message/Message.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Form, Col, Button, ButtonToolbar } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
+import './Message.css';
+
+class Message extends Component {
+
+  render() {
+    return (
+      <Modal
+        {...this.props}
+        size="md"
+        centered
+      >
+        <Modal.Body>
+          <Form.Row className="d-flex justify-content-center">
+            <Form.Group>
+              <h4>Message</h4>
+            </Form.Group>
+          </Form.Row>
+          <Form.Row className="d-flex justify-content-center">
+            <Form.Group>
+              Bạn đã đặt phòng thành công!
+            </Form.Group>
+          </Form.Row>
+          <Form.Row className="d-flex justify-content-center">
+            <Form.Group>
+              <Button variant="outline-primary btn-confirm" onClick={this.props.onHide}>OK</Button>
+            </Form.Group>
+          </Form.Row>
+        </Modal.Body>
+      </Modal>
+    );
+  }
+}
+
+export default Message;
