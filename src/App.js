43dc6e6b67a6475d0348fb32f10ba39b16412e25
darkflamemaster2321998 <darkflamemaster2321998@gmail.com>
2019-08-12 17:38:02 +0700

    refs #1 edit app.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react'
 import './App.css'
 import "react-datepicker/dist/react-datepicker.css";
 import RouterURL from './config/RouterURL';
-import { BrowserRouter as Router} from "react-router-dom";
+import { PrivateRoute } from './common/PrivateRoute';
+import { BrowserRouter as Router } from "react-router-dom";
 import HeaderHomeScreen from './components/HeaderHomeScreen';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
+    const isLogin = localStorage.getItem('token')
     return (
       <Router>
-        <HeaderHomeScreen />
+        {!isLogin && <HeaderHomeScreen />}
         <RouterURL />
       </Router>
-
     )
   }
 }
