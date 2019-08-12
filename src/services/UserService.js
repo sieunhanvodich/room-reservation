45396c7ba743afbe648afe55042c67ad6c8ada57
darkflamemaster2321998 <darkflamemaster2321998@gmail.com>
2019-08-12 17:39:11 +0700

    refs #1 edit user service

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,12 +1,52 @@
 import axios from './Axios'
 
-function getUsersInfo () {
-  return axios.get('/users')
-    .then(response => {
-      return response.data
-    })
+// function getUsersInfo () {
+//   return axios.get('/users')
+//     .then(response => {
+//       return response.data
+//     })
+// }
+
+// export default {
+//   getUsersInfo
+// }
+
+export const userService = {
+    login,
+    logout
+};
+
+function login(username, password) {
+    const request = JSON.stringify({ username, password })
+    return axios(`/login`, request)
+        .then(handleResponse)
+        .then(user => {
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            localStorage.setItem('token', JSON.stringify(user));
+
+            return user;
+        });
 }
 
-export default {
-  getUsersInfo
+function logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('token');
+}
+
+function handleResponse(response) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+        if (!response.ok) {
+            if (response.status === 401) {
+                // auto logout if 401 response returned from api
+                logout();
+                location.reload(true);
+            }
+
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+
+        return data;
+    });
 }
