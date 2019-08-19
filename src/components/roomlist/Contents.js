37591d28d2ba6ada3d43153af78cf8f3828bfd20
Le Thanh Dat (VTI.D2.Intern) <dat.lethanh@vti.vn>
2019-08-19 18:22:55 +0700

    update

--- src/components/roomlist/Contents.js
+++ src/components/roomlist/Contents.js
@@ -3,38 +3,89 @@ import { Tab, Tabs } from 'react-bootstrap';
 import DetailRoom from './DetailRoom';
 import axios from 'axios';
 
+
 class Contents extends Component {
   constructor(props) {
     super(props);
     this.state = {
       info: [],
-      key: 'IOT'
+      key: 'IOT',
+      bookTypeName: [],
+      timearr: [
+        {
+          start: '9:30',
+          end: '10:00',
+          type: ''
+        },
+        {
+          start: '10:00',
+          end: '10:30',
+          type: ''
+        },
+        {
+          start: '10:30',
+          end: '11:00',
+          type: ''
+        },
+        {
+          start: '11:00',
+          end: '11:30',
+          type: ''
+        },
+        {
+          start: '11:30',
+          end: '12:00',
+          type: ''
+        },
+        {
+          start: '12:00',
+          end: '12:30',
+          type: ''
+        },
+        {
+          start: '12:30',
+          end: '13:00',
+          type: ''
+        },
+        {
+          start: '13:00',
+          end: '13:30',
+          type: ''
+        },
+      ]
     }
+    
   }
 
   componentDidMount() {
     axios.get('http://localhost:3000/roomlist')
       .then(res => {
         const info = res.data.roomList;
+        const bookTypeName = res.data.bookTypeName;
         this.setState({ info });
-        console.log(this.state.info)
+        console.log(info)
+        this.setState({bookTypeName});
+        
+        // const a = this.state.timearr
+        // a[0].type = 'red'
+        // this.setState({ timearr: a})
       })
   }
-
+  
   onSelectTab = (key) => {
     this.setState({
       key
     })
   }
-
-
+  
+  
   render() {
     return (
-      <Tabs id="controlled-tab-example" activeKey={this.state.key} onSelect={key => this.onSelectTab(key)}>
+      <Tabs id="controlled-tab-example"  activeKey={this.state.key} onSelect={key => this.onSelectTab(key)}>
         {this.state.info.map((value, index) => {
           return (
             <Tab eventKey={value.name} title={value.name}>
-              <DetailRoom id="name" data={value} />
+              <DetailRoom id="name" data={value} timearr={this.state.timearr} />
 
             </Tab>
           )
--- src/components/roomlist/DetailRoom.js
+++ src/components/roomlist/DetailRoom.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Row, Col, Container, Image, Button, Badge } from 'react-bootstrap'
-
 import './DetailRoom.css'
 import { Link } from 'react-router-dom'
 
 class DetailRoom extends Component {
+
   render() {
     return (
       <Container className="container">
@@ -17,25 +17,11 @@ class DetailRoom extends Component {
           </Col>
           <Col xl={9} lg={9} xs={12}>
             <div className="flex-container">
-              <span>8:30-9:00</span>
-              <span>9:00-9:30</span>
-              <span>9:30-10:00</span>
-              <span>10:30-11:00</span>
-              <span>11:00-11:30</span>
-              <span>11:30-12:00</span>
-              <span>13:00-13:30</span>
-              <span>13:30-14:00</span>
-              <span>14:00-14:30</span>
-              <span>14:30-15:00</span>
-              <span>15:00-15:30</span>
-              <span>15:30-16:00</span>
-              <span>16:30-17:00</span>
-              <span>17:00-17:30</span>
-              <span>17:30-18:00</span>
-              <span>18:30-19:00</span>
-              <span>19:00-19:30</span>
-              <span>19:30-20:00</span>
-              
+              {this.props.timearr.map((value, index) => {
+                return (
+                  <span className={value.type}>{`${value.start} - ${value.end}`}</span>
+                )
+              })}
             </div>
           </Col>
         </Row>
@@ -45,22 +31,18 @@ class DetailRoom extends Component {
           <div className="describe">
             <h3 className="description">Description</h3>
             <h1>{this.props.data.name}</h1>
-            <div className="detail">
+            <div className="detail-room">
               <div>Position: {this.props.data.position}</div>
               <div>Capacity: {this.props.data.capacity} </div>
               <div>Detail: </div>
               <p>{this.props.data.description}</p>
-
               <div>
                 <Badge variant="success">Booked</Badge>
                 <Badge variant="danger">Daily Book</Badge>
                 <Badge variant="warning">Weekly Book</Badge>
                 <Badge variant="info">Monthly Book</Badge>
-               
-
               </div>
             </div>
-
           </div>
         </Row>
       </Container>
